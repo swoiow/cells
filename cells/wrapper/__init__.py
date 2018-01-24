7149268216c38d7ed8bf4063eab74a0601c5d9ed
swoiow <swoiow@users.noreply.github.com>
2018-01-24 10:21:12 +0800

    update

--- cells/wrapper/__init__.py
+++ cells/wrapper/__init__.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .authenticated import authenticated as login_required
+import functools
+import time
+
+
+def show_run_time(f):
+    @functools.wraps(f)
+    def swap(*args, **kwargs):
+        print("START TIME：", time.ctime())
+        c = time.time()
+        print("START TIME：", c)
+
+        f(*args, **kwargs)
+
+        print("LAST TIME：", time.time() - c)
+    return swap
