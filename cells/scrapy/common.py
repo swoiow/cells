9146e0fd311042c1389f9e09a90e3d58c835a819
swoiow <swoiow@users.noreply.github.com>
2018-03-26 00:53:52 +0800

    update

--- cells/scrapy/common.py
+++ cells/scrapy/common.py
@@ -6,6 +6,12 @@ import re
 
 import six.moves.urllib.parse as urlparse
 
+WHITE_LIST = ["html", "xhtml", "shtml", "htm", "php", "jsp", "cgi"]
+BLACK_LIST = [
+    "doc", "docx", "ppt", "pptx", "xls", "xlsx", "pdf", "tif", "zip", "rar", "jpg", "png", "gif", "jpeg", ".swf",
+    ".txt", ".img", ".ico", ".css", ".js", ".xml", ".svg", ".json", ".scss"
+]
+
 
 def format_url(uri):
     """fix no scheme url """
@@ -22,8 +28,10 @@ def fix_relative_url(uri, path="/"):
 
         new_uri_arr = urlparse.urlparse(new_uri)
         new_path = posixpath.normpath(new_uri_arr[2])
-        new_uri = urlparse.urlunparse((new_uri_arr.scheme, new_uri_arr.netloc, new_path,
-                                       new_uri_arr.params, new_uri_arr.query, new_uri_arr.fragment))
+        new_uri = urlparse.urlunparse((
+            new_uri_arr.scheme, new_uri_arr.netloc, new_path, new_uri_arr.params, new_uri_arr.query,
+            new_uri_arr.fragment
+        ))
 
         return new_uri
 
@@ -37,18 +45,15 @@ def file_or_not(uri, use="white_list", **kwargs):
     uri_arr = urlparse.urlparse(uri)
     end_prefix = uri_arr.path.lower().rsplit(".", 1)
 
-    white_list = kwargs.get("white_list") and kwargs["white_list"] \
-                 or ["html", "xhtml", "shtml", "htm", "php", "jsp", "cgi"]
-    black_list = kwargs.get("black_list") and kwargs["black_list"] \
-                 or ["doc", "docx", "ppt", "pptx", "xls", "xlsx", "pdf", "tif", "zip", "rar", "jpg", "png", "gif",
-                     "jpeg", "swf", "txt"]
+    white_list = kwargs.get("white_list") and kwargs["white_list"] or WHITE_LIST
+    black_list = kwargs.get("black_list") and kwargs["black_list"] or BLACK_LIST
 
     if use == "white_list":
         if end_prefix[-1] in white_list:
             return uri
 
     elif use == "black_list":
-        if end_prefix[-1] not in black_list:
+        if not any(map(lambda x: uri.lower().find(x) > -1, black_list)):
             return uri
 
     else:
