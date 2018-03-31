3e02f0cc6dd424a912b42327e312c948f6de1f5b
swoiow <swoiow@users.noreply.github.com>
2018-03-31 12:33:42 +0800

    update

--- cells/scrapy/middlewares.py
+++ cells/scrapy/middlewares.py
@@ -5,3 +5,4 @@ from __future__ import absolute_import
 
 from .src.RandomUserAgentMiddleware import RandomUserAgentMiddleware
 from .src.PuppeteerMiddleWare import PuppeteerMiddleWare
+from .src.ProxyMiddleware import ProxyMiddleware
