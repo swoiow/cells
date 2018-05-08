cc1d44e5c4d7754cb3a5943404662e236f7456bd
swoiow <swoiow@users.noreply.github.com>
2018-05-08 15:20:26 +0800

    update

--- cells/db/dbutils.py
+++ cells/db/dbutils.py
@@ -8,32 +8,64 @@
 from __future__ import absolute_import
 
 import os
+import sys
+import traceback
+from contextlib import contextmanager
+from datetime import datetime
 
-import sqlalchemy as sa
+from six.moves import cPickle
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm.session import Session
 
 __all_ = ["engine", "Session", "Base", "MyBase"]
 
-DB_URL = os.environ.get("DATABASE_URI")
 
-if not DB_URL:
-    raise Exception("没找到环境变量'DATABASE_URI'的值")
+@contextmanager
+def db_write(engine=None):
+    """Provide a transactional scope around a series of operations."""
 
-print("-" * 20)
-print(DB_URL)
-print("=" * 20)
+    engine = engine or os.environ.get("DATABASE_URI")
+    session = Session(bind=engine)
+    # Session = scoped_session(
+    #     sessionmaker(bind=engine, autocommit=False, autoflush=False)
+    # )
 
-engine = sa.create_engine(DB_URL, convert_unicode=True)
-Session = scoped_session(
-    sessionmaker(bind=engine, autocommit=False, autoflush=False)
-)
+    try:
+        yield session
+        session.commit()
+
+    except Exception as e:
+        session.rollback()
+        traceback.print_exc()
+        f_locals = sys.exc_info()[2].tb_next.tb_frame.f_locals
+
+        _v = {k: v for k, v in f_locals.items() if isinstance(v, (str, bytes, dict, list, tuple))}
+        _f = "session_{}.dmp".format(datetime.now().strftime("%Y%m%d%H%M%S_%f"))
+        _p = os.path.join(os.getcwd(), _f)
+        with open(_p, "wb") as wf:
+            cPickle.dump(obj=_v, file=wf)
+            print("Error and export dump in => {}".format(_p))
+
+    finally:
+        session.close()
+
+
+@contextmanager
+def db_read(engine=None):
+    engine = engine or os.environ.get("DATABASE_URI")
+    session = Session(bind=engine)
+
+    try:
+        yield session
+    finally:
+        session.close()
+
 
 Base = declarative_base()
 
 
 class MyORMBase(object):
-    query = Session.query_property()
+    # query = Session.query_property()
 
     def row2dict(self, r):
         return {c.name: str(getattr(r, c.name)) for c in r.__table__.columns}
