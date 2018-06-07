211e7e2978773176ffb9589d4c88dea41c09d647
swoiow <swoiow@users.noreply.github.com>
2018-06-07 14:50:50 +0800

    update

--- cells/scrapy/src/LoggerExtension.py
+++ cells/scrapy/src/LoggerExtension.py
@@ -9,11 +9,11 @@ from .logger import init_logging
 class LoggerExtension(object):
     @classmethod
     def from_crawler(cls, crawler):
-        ph = crawler.settings.get("LOG_PATH", "/var/log")
+        ph = crawler.settings.get("LOG_PATH", "/var/log/scrapy")
         makedirs(ph, exist_ok=True)
 
         init_logging(
-            bot_name=crawler.settings.get("BOT_NAME", "default"),
+            bot_name=crawler.settings.get("BOT_NAME", "extDefault"),
             path=ph,
             level=crawler.settings.get("LOG_LEVEL", "INFO"),
             encoding=crawler.settings.get("LOG_ENCODING", "gbk"),
