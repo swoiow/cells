5b85c4c99d7de982ea0e352257ada86af482b966
swoiow <swoiow@users.noreply.github.com>
2018-07-24 11:25:23 +0800

    update logger

--- cells/helper/logger/logger.py
+++ cells/helper/logger/logger.py
@@ -7,33 +7,33 @@ import logging
 from logging.handlers import (HTTPHandler, RotatingFileHandler, TimedRotatingFileHandler)
 from os.path import curdir, join
 
-_FORMATTER = " %(levelname).1s %(asctime)s [%(name)s] [%(filename)s#L%(lineno)d] %(message)s"
+_FORMATTER = " %(levelname).1s %(asctime)s [%(name)s:%(filename)s] L%(lineno)d > %(message)s"
 _MAX_BYTES = 10 * 1024 * 1024  # 10M
 _BACKUP_COUNT = 10
 _FILE_DELAY = True
 _ENCODING = "utf8"
 
 
-def add_handle(_func):
-    @functools.wraps(_func)
+def add_handle(func):
+    @functools.wraps(func)
     def swap(self, *args, **kwargs):
-        func = _func(self)
+        _func = func(self)
 
-        origin_keys = [v for v in inspect.getfullargspec(func).args if v not in ["self"]]
-        origin_defaults = inspect.getfullargspec(func).defaults
+        origin_keys = [v for v in inspect.getfullargspec(_func).args if v not in ["self"]]
+        origin_defaults = inspect.getfullargspec(_func).defaults
         origin_params = zip(origin_keys[::-1][:len(origin_defaults)], origin_defaults[::-1])
 
         # 对外部调用时的 kwargs 参数, 进行预处理
         prepare_params = self.prepare(**kwargs)
         # 与原函数的默认值，和预处理结果进行合并
-        mix_params = {**dict(origin_params), **prepare_params}
+        mix_params = {**prepare_params, **dict(origin_params)}
         # 根据调用的 keys, 组合参数
         params = {p: mix_params.get(p) for p in origin_keys}
 
-        hd = func(**params)
+        hd = _func(**params)
         hd.name = "{cls}@{name}: lv{level}".format(
             cls=hd.__class__.__name__,
-            name=func.__name__,
+            name=_func.__name__,
             level=params.get("level", logging.INFO)
         )
 
@@ -43,7 +43,13 @@ def add_handle(_func):
         if "formatter" in prepare_params:
             hd.setFormatter(prepare_params["formatter"])
 
+        if "hd_name" in prepare_params:
+            hd.name = prepare_params["hd_name"]
+
         self.inst._call_add_handler(hd)
+
+        # 删除不再需要的对象
+        del origin_keys, origin_defaults, origin_params, prepare_params, mix_params
 
     return swap
 
@@ -62,9 +68,9 @@ class _Handles(object):
         backupCount = backup_count = kwargs.get("backupCount", kwargs.get("backup_count", _BACKUP_COUNT))
         fileDelay = file_delay = kwargs.get("fileDelay", kwargs.get("file_delay", _FILE_DELAY))
 
-        nm = kwargs.get("name", self.inst.name)
+        nm = kwargs.get("name") and kwargs["name"] or self.inst.name
         ph = kwargs.get("path", curdir)
-        name = "default-{}.log".format(nm)
+        name = "{}".format(nm)
         filename = path = join(ph, name)
 
         level = kwargs.get("level")
@@ -72,26 +78,36 @@ class _Handles(object):
             level = getattr(logging, level.upper())
 
         level = level or self.inst.level
-        _formatter = kwargs.get("formatter", _FORMATTER)
-        formatter = logging.Formatter(_formatter)
+        formatter = kwargs.get("formatter", _FORMATTER)
+        formatter = logging.Formatter(formatter)
 
-        return locals()
+        return {**kwargs, **locals()}
 
     @add_handle
-    def RotatingFileHandler(self, name=None, path=curdir, level=None, formatter=None, encoding=_ENCODING, **kwargs):
+    def lgg_RotatingFileHandler(self, **kwargs):
         return RotatingFileHandler
 
     @add_handle
-    def TimedRotatingFileHandler(self, name=None, path=None, level=None, formatter=None, encoding=_ENCODING, **kwargs):
+    def lgg_TimedRotatingFileHandler(self, **kwargs):
         return TimedRotatingFileHandler
 
     @add_handle
-    def HTTPHandler(self, **kwargs):
+    def lgg_HTTPHandler(self, **kwargs):
         return HTTPHandler
 
     @add_handle
-    def Console(self, level=None, formatter=None, stream=None, **kwargs):
+    def lgg_Console(self, **kwargs):
         return logging.StreamHandler
+
+    @add_handle
+    def lgg_KafkaHandler(self, **kwargs):
+        from .handles.KafkaHandler import KafkaHandle
+        return KafkaHandle
+
+    @add_handle
+    def lbk_TimedRotatingFileHandler(self, **kwargs):
+        from logbook.handlers import TimedRotatingFileHandler
+        return TimedRotatingFileHandler
 
 
 class InitLogger(logging.Logger):
