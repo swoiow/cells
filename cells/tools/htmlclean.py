edb1447bc5800a67b5012e0bc97b07314702fdff
swoiow <swoiow@users.noreply.github.com>
2018-10-10 10:19:45 +0800

    update

--- cells/tools/htmlclean.py
+++ cells/tools/htmlclean.py
@@ -1,11 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import bleach
+
 from functools import partial
-from blogsettings import cfg
+
+try:
+    import bleach
+    from lxml.html import defs
+
+except ImportError as e:
+    from colorama import Fore
+
+    print(Fore.RED + e.msg)
+    exit(127)
 
 __all__ = ["HtmlClean"]
+
+cfg = {
+    "tags": defs.block_tags,
+    "attrs": {
+        "*": defs.safe_attrs,
+        "a": ["href", "title"],
+        "acronym": ["title"],
+        "abbr": ["title"],
+        "img": ["alt"],
+        "applet": ["code", "object"],
+        "embed": ["src"],
+        "iframe": ["src"],
+        "layer": ["src"],
+        "link": ["href"],
+        "script": ["src"],
+    },
+    "protocols": ["http", "https", "mailto", "sss"],
+    "style": [
+        "color", "font-size", "font-style", "font-weight", "margin", "margin-left", "text-align",
+        "text-decoration",
+    ],
+}
 
 HtmlClean = partial(
     bleach.clean,
