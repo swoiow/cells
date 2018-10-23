dd30a875c20a237ed77d57511f8aa9f9ac8b13e6
Cell <swoiow@users.noreply.github.com>
2018-10-23 09:58:48 +0800

    Update extensions.py Rename FilterMiddleware.py to FilterUrls.py

--- cells/scrapy/extensions.py
+++ cells/scrapy/extensions.py
@@ -3,5 +3,5 @@
 
 from __future__ import absolute_import
 
-from .src.FilterMiddleware import BLOOMDupeFilter
+from .src.FilterUrls import BLOOMDupeFilter
 from .src.LoggerExtension import LoggerExtension
