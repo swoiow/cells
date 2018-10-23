254b90f022689870ed2b23f6593cf9708399d5f1
swoiow <swoiow@users.noreply.github.com>
2018-10-23 11:13:09 +0800

    [update] fix_relative_url

--- cells/scrapy/common.py
+++ cells/scrapy/common.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import logging
+import difflib
 import posixpath
 import re
 
 import six.moves.urllib.parse as urlparse
+
 
 _doc_file_list = ["txt", "doc", "docx", "ppt", "pptx", "xls", "xlsx", "pdf", ]
 _zip_file_list = ["zip", "rar", "7z", "tar", "tar.bz", "tar.gz", "xz"]
@@ -21,6 +22,7 @@ DEFAULT_RULE = re.compile(_rule.format("|".join(_web_file_list + _img_file_list)
 
 def format_url(uri):
     """ [deprecated] fix no scheme url """
+
     uri = re.sub(r'''[\\"']''', "", uri)
     uri = urlparse.urlparse(uri, scheme="http")
     if uri.scheme in ["http", "https"]:
@@ -45,24 +47,41 @@ def fix_scheme(uri: str, default_scheme="http"):
     return uri
 
 
-def fix_relative_url(uri, path):
+def fix_relative_url(uri, path, debug=False):
     """
         print(fix_relative_url('http://localhost.com', 'http://localhost.com/aa'))
         print(fix_relative_url('http://localhost.com', '//localhost.com/bb'))
         print(fix_relative_url('http://localhost.com', 'www.localhost.com'))
+        print(fix_relative_url("http://localhost.com", "sub1.sub2.localhost.cn/this-is-a-longer-post-title.html"))
+
         print("-" * 3)
         print(fix_relative_url('http://localhost.com', '/phA/phB'))
         print(fix_relative_url('http://localhost.com', './phA/phB'))
         print("-" * 3)
-        print(fix_relative_url('http://localhost.com/phA', '../phA/a.jpg'))  # nothings to do, because this is wrong url
+        print(fix_relative_url('http://localhost.com/phA', '../phA/a.jpg'))
         print(fix_relative_url('http://localhost.com/phA/phB/', '../phC/phD'))
         print("-" * 3)
         print(fix_relative_url('http://domainA.com', 'http://www.domainB.com'))
         print(fix_relative_url('http://domainA.com', 'www.domainB.com/phA'))
+        print(fix_relative_url('http://domainA.com', 'phA'))
+        print(fix_relative_url('http://domainA.com', 'java'))
     """
-    if (not path.startswith("/")) and (not path.startswith(".")):
-        logging.debug("Nothings to do: fix failed with [{}]&[{}]".format(uri, path))
-        return fix_scheme(path)
+
+    if path.lower().startswith("javascript:"):
+        return None
+
+    elif (not path.startswith("/")) and (not path.startswith(".")):
+        if debug:
+            print("[W] => fix_relative_url: failed with `{}` & `{}`".format(uri, path))
+
+        len_uri, len_path = len(uri), len(path)
+        weights = len_path > len_uri and 1 + len_path // len_uri or 0
+
+        seq = difflib.SequenceMatcher(None, uri, path[:len_uri + weights])
+        if seq.ratio() > 0.5:
+            return fix_scheme(path)
+        else:
+            return urlparse.urljoin(uri, path)
 
     elif path.startswith("//"):
         return fix_scheme(path)
