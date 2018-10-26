67dfbe9f91c2c24369ba796d8d1d6ed314c0e4a5
swoiow <swoiow@users.noreply.github.com>
2018-10-27 01:23:21 +0800

    add DefaultPipelines

---
+++ cells/scrapy/piplines.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .src.DefaultPipelines import Pipeline as DefaultPipeline
---
+++ cells/scrapy/src/DefaultPipelines.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+from os import environ
+
+import sqlalchemy as sa
+from celorm.utils import db_read, db_write
+
+
+class Pipeline(object):
+    INNER_TIME = time.time()
+
+    def open_spider(self, spider):
+        sa_url = environ["sa_url"]
+
+        self.ENG = sa.create_engine(sa_url, strategy='threadlocal')
+        self.INST_BUCKET = []
+
+    def process_item(self, item, spider):
+        model = spider.model
+
+        o = model(**item)
+        with db_read(self.ENG) as db:
+            if db.query(model.id).filter(model.hash == o.hash).count() < 1:
+                self.INST_BUCKET.append(o)
+
+        if time.time() > self.INNER_TIME + 60:
+            with db_write(self.ENG) as db:
+                db.bulk_save_objects(self.INST_BUCKET)
+
+                spider.logger.info(
+                    "Heartbeat: %s => %s" % (time.ctime(), len(self.INST_BUCKET))
+                )
+
+            self.INST_BUCKET.clear()
+            self.INNER_TIME = time.time()
+
+        return item
+
+    def close_spider(self, spider):
+        with db_write(self.ENG) as db:
+            db.bulk_save_objects(self.INST_BUCKET)
+
+        self.INST_BUCKET.clear()
+
+        spider.logger.info("close_spider ... 88")
