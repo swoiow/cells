ed3fe37061aa1b48f8bce913d17c86889d97f828
swoiow <swoiow@users.noreply.github.com>
2018-12-15 14:32:15 +0800

    update

--- cells/mods/pkf/__main__.py
+++ cells/mods/pkf/__main__.py
@@ -2,25 +2,42 @@
 # -*- coding: utf-8 -*
 
 
-def tar_object(pwd, filename=None, mode=None, enc_mode=None):
+def tar_object(pwd, filename=None, mode=None, enc_mode=None, gz=False):
     import os, uuid
+
+    tar_mod, end = "J", "xz"
+    if gz:
+        tar_mod, end = "z", "gz"
 
     if not filename:
         filename = "*"
     if not enc_mode:
         enc_mode = "aes192-wrap"
 
+    plus = "-pbkdf1"
+    check_openssl_version = os.popen("openssl version").read()
+    if check_openssl_version.find(" 1.1") > 0:
+        plus = "-pbkdf2"
+
     if mode == "e":
-        c = "tar -czf - {fn} | openssl enc -{em} -pbkdf2 -e -k {pwd} | dd of=_{fid}.enc"
-        c = c.format(fn=filename, pwd=pwd, fid=uuid.uuid4().hex, em=enc_mode)
+        c = "tar c{mod}f - {fn} | openssl enc -{em} {plus} -e -k {pwd} | dd of=_{fid}.enc.{end}"
+        c = c.format(
+            fn=filename, pwd=pwd, mod=tar_mod, fid=uuid.uuid4().hex,
+            em=enc_mode, plus=plus, end=end
+        )
         print(c)
 
         os.system(c)
         print("Encrypt Finish !!")
 
     elif mode == "d":
-        c = "dd if={fn} | openssl enc -{em} -pbkdf2 -d -k {pwd} | tar zxf -"
-        c = c.format(fn=filename, pwd=pwd, em=enc_mode)
+        tar_mod = "z" if filename.endswith("gz") else "J"
+
+        c = "dd if={fn} | openssl enc -{em} {plus} -d -k {pwd} | tar x{mod}f -"
+        c = c.format(
+            fn=filename, pwd=pwd, mod=tar_mod,
+            em=enc_mode, plus=plus
+        )
         print(c)
 
         os.system(c)
@@ -40,9 +57,12 @@ def mod(args=None):
     parser.add_argument("-p", dest="pwd", metavar="pwd", required=True,
                         help="Encrypt password .")
     parser.add_argument("-m", dest="mode", metavar="mode", required=False, default="e",
-                        help="Encrypt(e) / Decrypt(d), default: e  .")
+                        help="Encrypt(e) / Decrypt(d), default: e .")
     parser.add_argument("-E", dest="enc_mode", metavar="enc_mode", required=False, default="aes192",
                         help="See 'openssl enc -h' .")
+    parser.add_argument("--gz", dest="gz", required=False, default=None,
+                        action='store_true',
+                        help="Tar file with gz mode. default xz .")
 
     options = parser.parse_args(args)
 
