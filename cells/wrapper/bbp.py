02829bf7537193fb6cbb917eab215e64f2db0416
swoiow <swoiow@users.noreply.github.com>
2019-02-27 02:42:58 +0800

    simple producer consumer class

---
+++ cells/wrapper/bbp.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" https://en.wikipedia.org/wiki/Producer%E2%80%93consumer_problem
+
+q = Queue()
+WORKER = 10
+def TASKS():
+    pass
+
+Producer.main(queue=q, tasks=TASKS)
+Consumer.main(queue=q, worker_func=ft, task_number=WORKER)
+"""
+
+import threading
+
+from six.moves.queue import Queue
+
+
+class Producer(threading.Thread):
+
+    def __init__(self, t_name, queue: Queue, tasks: (list, iter, tuple)):
+        self.queue = queue
+        self._tasks = tasks
+
+        threading.Thread.__init__(self, name=t_name)
+
+    def run(self):
+        for t in self._tasks:
+            self.queue.put(t)
+
+    @staticmethod
+    def main(queue: Queue, tasks: (list, iter, tuple)):
+        producer = Producer("producer", queue, tasks=tasks)
+        producer.start()
+        producer.join()
+
+        print("Producer is under running!")
+
+
+class Consumer(threading.Thread):
+
+    def __init__(self, t_name, queue: Queue, func_handle_task: callable):
+        self.queue = queue
+        self._func_handle_task = func_handle_task
+        threading.Thread.__init__(self, name=t_name)
+
+    def run(self):
+        self._func_handle_task(self.queue)
+
+    @staticmethod
+    def main(worker_func: callable, queue: Queue, task_number: int):
+        for num in range(task_number):
+            consumer = Consumer("consumer", queue, func_handle_task=worker_func)
+            consumer.setDaemon(True)
+
+            consumer.start()
+            print("Consumer-{} is running!".format(num))
+
+        consumer.join()
